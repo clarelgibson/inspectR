38511a34fa058746d489a22012d61a3c78633dbf
Clare Gibson <clarelgibson@gmail.com>
2025-09-27 10:26:43 +0100

    Setup

---
+++ 04_notebooks/utils.R
@@ -0,0 +1,10 @@
+# Contains global elements to be used across multiple notebooks
+
+# Load packages
+library(knitr)
+library(here)
+library(tidyverse)
+library(readxl)
+library(janitor)
+library(lubridate)
+library(skimr)
